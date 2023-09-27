1ad7e1e766d9ddf1cb5ceccc85fdd79485197c02
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-09-27 15:22:09 -0700

    three step

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log('hello,world')
+
+function addMovie(event){
+  event.preventDefault()
+  let inputField = document.querySelector('input')
+  let movie = document.createElement('li')
+  let movieTitle = document.createElement('span')
+  movie.textContent = inputField.value
+  movie.appendChild(movieTitle)
+  let deleteBtn = document.createElement('button')
+  movie.appendChild(deleteBtn)
+  deleteBtn.textContent = 'x'
+  deleteBtn.addEventListener('click', deleteMovie) 
+  document.querySelector('ul').appendChild(movie)
+  inputField.value = ''
+}
+
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
+function deleteMovie(event){
+  event.target.parentNode.remove()
+}
+
